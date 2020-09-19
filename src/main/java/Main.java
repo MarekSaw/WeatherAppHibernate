94b9d2446993da7843c508da7d4bdae93f975e11
Marek <marek.sawick@gmail.com>
2020-09-19 19:39:23 +0200

    refactorized

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,7 @@
-import Proccesing.Forecast;
+
 import Proccesing.JSONForecastProcessing;
 import Repositories.OpenWeatherRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
-
-import java.io.IOException;
 import java.time.LocalDate;
 
 public class Main {
@@ -13,10 +10,11 @@ public class Main {
 
         OpenWeatherRepository owr = new OpenWeatherRepository("8f8e09e8c64b12495e3cfdb7f75b54b1");
         JSONForecastProcessing json = new JSONForecastProcessing();
-        System.out.println(owr.getForecast(53.133331, 23.15));
 
-        System.out.println(owr.getForecast("London",LocalDate.now().plusDays(2)));
-        System.out.println(owr.getForecast("London"));
+        System.out.println(owr.getForecast("Białystok",LocalDate.now().plusDays(2)));
+        System.out.println(owr.getForecast("Białystok"));
+        System.out.println(owr.getForecast(53.133331, 23.15,LocalDate.now().plusDays(2)));
+        System.out.println(owr.getForecast(53.133331, 23.15));
 
     }
 }
