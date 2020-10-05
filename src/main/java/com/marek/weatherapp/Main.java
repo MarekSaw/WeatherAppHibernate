a757bab4f66655e74ef70af3487d268f666800a6
Marek <marek.sawick@gmail.com>
2020-10-05 12:43:46 +0200

    Main App with Command Line Arguments done

--- src/main/java/com/marek/weatherapp/Main.java
+++ src/main/java/com/marek/weatherapp/Main.java
@@ -1,78 +1,76 @@
 package com.marek.weatherapp;
 
-import com.marek.weatherapp.entities.ForecastEntity;
 import com.marek.weatherapp.entities.WeatherForecastEntity;
-import com.marek.weatherapp.forecastcache.ForecastDao;
-import com.marek.weatherapp.forecastcache.WeatherForecastDao;
-import com.marek.weatherapp.proccesing.JsonProcessing;
-import com.marek.weatherapp.repositories.model.WeatherSource;
 import com.marek.weatherapp.repositories.model.openweather.OpenWeatherRepository;
+import org.apache.commons.cli.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Scanner;
-
+import java.util.InputMismatchException;
+import java.util.Objects;
 
 public class Main {
 
 
     public static void main(String[] args) {
 
-        LocalDate tomorrow = LocalDate.now().plusDays(1);
-
-        //SessionFactory sessionFactory = DatabaseConfig.getSessionFactory();
-        //sessionFactory.getCurrentSession();
-
-
-
-
         OpenWeatherRepository owr = new OpenWeatherRepository(OpenWeatherRepository.readKey());
-        ForecastDao wfd = new ForecastDao();
-        ForecastEntity forecast = new ForecastEntity();
 
-        WeatherForecastEntity forecastForCity1 = owr.getForecast("London");
-        WeatherForecastEntity forecastForCity2 = owr.getForecast("London");
-
-        System.out.println(forecastForCity1);
+//        WeatherForecastEntity forecastForCity1 = owr.getForecast("London");
+        WeatherForecastEntity forecastForCity2 = owr.getForecast(50.041187, 21.999121, LocalDate.now().plusDays(2));
+//        System.out.println(forecastForCity1);
         System.out.println(forecastForCity2);
-
-
-
-
-
-
-        /*Scanner input = new Scanner(System.in);
-        System.out.println("Witaj, jeżeli chcesz uzyskać informacje o pogodzie na podstawie nazwy miasta wpisz 1, jeżeli koordynatów to 2");
-        switch (input.nextInt()) {
-            case 1:
-                System.out.println("Podaj nazwę miasta");
-                String city = input.next();
-                System.out.println("Podaj datę w formacie rok, miesiac, dzien jako liczby całkowite kolejno zatwierdzając przyciskiem enter");
-                System.out.println("Jeżeli nie chcesz podawać wpisz 0");
-                int year = input.nextInt();
-                if(year == 0){
-                    System.out.println(owr.getForecast(city));
-                }else{
-                    System.out.println(owr.getForecast(city, LocalDate.of(year,input.nextInt(),input.nextInt())));
-                }
-                break;
-            case 2:
-                System.out.println("Podaj koordynaty miasta w kolejności szerokość, długość");
-                double lat = input.nextDouble();
-                double lon = input.nextDouble();
-                System.out.println("Podaj datę w formacie rok, miesiac, dzien jako liczby całkowite kolejno zatwierdzając przyciskiem enter");
-                System.out.println("Jeżeli nie chcesz podawać daty wciśnij enter");
-                int year1 = input.nextInt();
-                if(year1 == 0){
-                    System.out.println(owr.getForecast(lat, lon));
-                }else{
-                    System.out.println(owr.getForecast(lat, lon,LocalDate.of(year1,input.nextInt(),input.nextInt())));
-                }
-                break;*/
-        }
+        callWithCommandLineArguments(owr, args);
 
 
     }
+
+    private static void callWithCommandLineArguments(OpenWeatherRepository openWeatherRepository, String[] args) {
+        CommandLine commandLine = parseCommandLine(args);
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+        WeatherForecastEntity weatherForecastEntity;
+
+        if (Objects.nonNull(commandLine)) {
+            // get arguments from command line
+            String city = commandLine.hasOption("c") ? commandLine.getOptionValue("c") : null;
+            Double lat = commandLine.hasOption("lat") ? Double.valueOf(commandLine.getOptionValue("lat")) : null;
+            Double lon = commandLine.hasOption("lon") ? Double.valueOf(commandLine.getOptionValue("lon")) : null;
+            LocalDate date = commandLine.hasOption("d") ? LocalDate.parse(commandLine.getOptionValue("d")) : tomorrow;
+
+            // check if localization is given properly
+            if (city == null && (lat == null || lon == null)) {
+                throw new InputMismatchException("Localization has been given incorrectly. City or both coordinates must be given");
+            }
+
+            // get forecast for given localization and date
+            if (city != null) {
+                weatherForecastEntity = openWeatherRepository.getForecast(city, date);
+            } else {
+                weatherForecastEntity = openWeatherRepository.getForecast(lat, lon, date);
+            }
+
+            System.out.println(weatherForecastEntity + " for date " + date);
+
+        }
+
+    }
+
+    private static CommandLine parseCommandLine(String[] args) {
+        try {
+            CommandLineParser parser = new GnuParser();
+            Options options = new Options();
+            options.addOption("d", "date", true, "Date to get forecast for");
+            options.addOption("c", "city", true, "City to get forecast for");
+            options.addOption("lat", "latitude", true, "Latitude coordinate to get forecast for");
+            options.addOption("lon", "longitude", true, "Longitude coordinate to get forecast for");
+            return parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println("An error occurred during argument parsing");
+            return null;
+        }
+    }
+
+
+}
 
 
 
